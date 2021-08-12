ee00d0e50daf28735256592bcbf2d14ed032834b
Júlia Brito <julialbritto@gmail.com>
2021-08-12 15:54:58 -0300

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AppTabRoutes } from './app.tab.routes'; 
 import { useAppTheme } from '../hooks/appTheme';
-import { ThemeProvider } from 'styled-components/native';
 
 export function Routes() {
   const { appTheme } = useAppTheme();
